8893daced80303ab46dfdbb5257cf78359620efd
François Beausoleil <francois@teksol.info>
2009-10-31 00:14:51 -0400

    Correctly parse error backtraces from Test::Unit.

--- lib/nestor/strategies/test/unit.rb
+++ lib/nestor/strategies/test/unit.rb
@@ -62,15 +62,14 @@ module Nestor
           filename = if failure.respond_to?(:location) then
                        failure.location.map do |loc|
                          filename = loc.split(":", 2).first
-                         data = test_files.detect {|tf| filename.include?(tf)}
-                         data
+                         test_files.detect {|tf| filename.include?(tf)}
                        end.compact.first
                      elsif failure.respond_to?(:exception) then
-                       failure.exception.backtrace.detect do |loc|
+                       failure.exception.backtrace.map do |loc|
                          filename = loc.split(":", 2).first
                          loc = loc[1..-1] if loc[0,1] == "/"
                          test_files.detect {|tf| filename.include?(tf)}
-                       end
+                       end.compact.first
                      else
                        raise "Unknown object type received as failure: #{failure.inspect} doesn't have #exception or #location methods."
                      end
@@ -89,7 +88,7 @@ module Nestor
           info = {"status" => test_runner.passed? ? "successful" : "failed", "failures" => {}}
           failures = info["failures"]
           test_runner.faults.each do |failure|
-            filename, test_name = self.class.parse_failure(failure)
+            filename, test_name = self.class.parse_failure(failure, test_files)
             if filename.nil? then
               log("Could not map #{failure.test_name.inspect} to a specific test file: mapping to #{test_files.length}")
               test_files.each do |tf|
--- spec/test_unit_strategy_spec.rb
+++ spec/test_unit_strategy_spec.rb
@@ -18,4 +18,24 @@ describe Nestor::Strategies::Test::Unit, "#parse_failure" do
     filename.should  == "test/functional/api/templates_controller_test.rb"
     test_name.should == "test: Api::TemplatesController should flunk"
   end
+
+  it "should return the file and test name as reported by an error" do
+    exception = RuntimeError.new("bad")
+    exception.set_backtrace(["./test/functional/api/templates_controller_test.rb:12:in `__bind_1256962198_402597'",
+                             "/Library/Ruby/Gems/1.8/gems/thoughtbot-shoulda-2.10.2/lib/shoulda/context.rb:351:in `call'",
+                             "/Library/Ruby/Gems/1.8/gems/thoughtbot-shoulda-2.10.2/lib/shoulda/context.rb:351:in `test: Api::TemplatesController should flunk. '",
+                             "/Library/Ruby/Gems/1.8/gems/activesupport-2.3.4/lib/active_support/testing/setup_and_teardown.rb:62:in `__send__'",
+                             "/Library/Ruby/Gems/1.8/gems/activesupport-2.3.4/lib/active_support/testing/setup_and_teardown.rb:62:in `run'"])
+
+    failure = mock
+    failure.stub(:exception).and_return(exception)
+    failure.stub(:test_name).and_return("test: Api::TemplatesController should flunk. (Api::TemplatesControllerTest)")
+
+    test_files = ["test/functional/api/templates_controller_test.rb"]
+
+    filename, test_name = Nestor::Strategies::Test::Unit.parse_failure(failure, test_files)
+
+    filename.should  == "test/functional/api/templates_controller_test.rb"
+    test_name.should == "test: Api::TemplatesController should flunk"
+  end
 end
