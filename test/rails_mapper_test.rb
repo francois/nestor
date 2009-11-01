e851afd58a863ce7f9ec2d68a5cb6b279953fe73
François Beausoleil <francois@teksol.info>
2009-11-01 09:11:34 -0500

    Use double quotes: they're my preference

--- test/rails_mapper_test.rb
+++ test/rails_mapper_test.rb
@@ -6,16 +6,16 @@ context "Nestor::Mappers::Rails" do
 
   context "#map" do
     should_map \
-      'app/models/user.rb'                       => ["test/unit/user_test.rb"],
-      'app/models/topic.rb'                      => ["test/unit/topic_test.rb"],
-      'app/controllers/users_controller.rb'      => ["test/functional/users_controller_test.rb"],
-      'app/views/users/index.html.erb'           => ["test/functional/users_controller_test.rb"],
-      'lib/core_ext/rails/hash.rb'               => ["test/unit/core_ext/rails/hash_test.rb"],
-      'db/schema.rb'                             => ["db/schema.rb"],
-      'test/unit/user_test.rb'                   => ["test/unit/user_test.rb"],
-      'test/functional/users_controller_test.rb' => ["test/functional/users_controller_test.rb"],
-      'test/integration/signup_test.rb'          => ["test/integration/signup_test.rb"],
-      'test/performance/home_page_test.rb'       => ["test/performance/home_page_test.rb"],
-      'app/models/timeline_observer.rb'          => ["test/unit/timeline_observer_test.rb", "test/unit/timeline_test.rb"]
+      "app/models/user.rb"                       => ["test/unit/user_test.rb"],
+      "app/models/topic.rb"                      => ["test/unit/topic_test.rb"],
+      "app/controllers/users_controller.rb"      => ["test/functional/users_controller_test.rb"],
+      "app/views/users/index.html.erb"           => ["test/functional/users_controller_test.rb"],
+      "lib/core_ext/rails/hash.rb"               => ["test/unit/core_ext/rails/hash_test.rb"],
+      "db/schema.rb"                             => ["db/schema.rb"],
+      "test/unit/user_test.rb"                   => ["test/unit/user_test.rb"],
+      "test/functional/users_controller_test.rb" => ["test/functional/users_controller_test.rb"],
+      "test/integration/signup_test.rb"          => ["test/integration/signup_test.rb"],
+      "test/performance/home_page_test.rb"       => ["test/performance/home_page_test.rb"],
+      "app/models/timeline_observer.rb"          => ["test/unit/timeline_observer_test.rb", "test/unit/timeline_test.rb"]
   end
 end
